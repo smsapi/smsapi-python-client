42e0126ea8ffc6b6eb5d3cfe7d035569b18a78cd
invoid <k.baran@smsapi.pl>
2016-08-22 13:57:43 +0200

    Bump version number to 1.1.2

--- smsapi/__init__.py
+++ smsapi/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'SmsApi'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'SMSAPI'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 SMSAPI'
